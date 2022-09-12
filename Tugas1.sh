de491996244e390e191567b87f6758b65ce78ca4
aisyah kirana <96468515+aisyahkirana@users.noreply.github.com>
2022-09-12 12:19:46 +0700

    Add files via upload

---
+++ Tugas1.sh
@@ -0,0 +1,10 @@
+#!/bi/bash
+printf '%.0s-' {1..35}
+echo -e "\nTugas 1 SISOP Kelas-A"
+printf '%.0s-' {1..35}
+echo -e "\nNama : Aisyah Kirana Putri Isyanto"
+echo "NPM : 21083010065"
+printf '%.0s-' {1..35}
+echo -e "\nnotes:"
+printf '%.0s-' {1..35}
+echo -e "\nHalo dunia!\nIni adalah skrip Bash pertamaku di Linux!"
